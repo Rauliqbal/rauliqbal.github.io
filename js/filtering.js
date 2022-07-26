7ceae8b7932548b7ac1d1609d0ff934a82d9954a
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-07-26 09:09:07 +0700

    add filter portfolio

--- js/filtering.js
+++ js/filtering.js
@@ -22,3 +22,14 @@ $(document).ready(function () {
    }
    $(this).addClass("active");
 });
+
+function toggleActive(event) {
+   var target = event.target || event.srcElement;
+   var buttonList = document.querySelectorAll(".toggle-active");
+   buttonList.forEach(function (button) {
+      if (button === target && !button.classList.contains("active")) {
+         return button.classList.add("active");
+      }
+      return button.classList.remove("active");
+   });
+}
