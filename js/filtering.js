dff615a4a26498974d6ace2847c7b73442dd7cb9
Rauliqbal <hgostline.gt@gmail.com>
2022-08-05 11:23:21 +0700

    [ADD]Portfolio

--- js/filtering.js
+++ js/filtering.js
@@ -1,35 +1,24 @@
-$(document).ready(function () {
-   $(".filter-button").click(function () {
-      var value = $(this).attr("data-filter");
+let sortBtn = document.querySelector(".filter-menu").children;
+let sortItem = document.querySelector(".filter-item").children;
 
-      if (value == "all") {
-         //$('.filter').removeClass('hidden');
-         $(".filter").show("1000");
-      } else {
-         //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-         //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-         $(".filter")
-            .not("." + value)
-            .hide("3000");
-         $(".filter")
-            .filter("." + value)
-            .show("3000");
+for (let i = 0; i < sortBtn.length; i++) {
+   sortBtn[i].addEventListener("click", function () {
+      for (let j = 0; j < sortBtn.length; j++) {
+         sortBtn[j].classList.remove("current");
       }
-   });
 
-   if ($(".filter-button").removeClass("active")) {
-      $(this).removeClass("active");
-   }
-   $(this).addClass("active");
-});
+      this.classList.add("current");
 
-function toggleActive(event) {
-   var target = event.target || event.srcElement;
-   var buttonList = document.querySelectorAll(".toggle-active");
-   buttonList.forEach(function (button) {
-      if (button === target && !button.classList.contains("active")) {
-         return button.classList.add("active");
+      let targetData = this.getAttribute("data-target");
+
+      for (let k = 0; k < sortItem.length; k++) {
+         sortItem[k].classList.remove("active");
+         sortItem[k].classList.add("delete");
+
+         if (sortItem[k].getAttribute("data-item") == targetData || targetData == "all") {
+            sortItem[k].classList.remove("delete");
+            sortItem[k].classList.add("active");
+         }
       }
-      return button.classList.remove("active");
    });
 }
