157ab8c0ca248fe707bb01adb7259e3b4b9214c9
Rauliqbal <hgostline.gt@gmail.com>
2022-08-08 10:41:59 +0700

    add portfolio

--- js/filtering.js
+++ js/filtering.js
@@ -1,47 +1,24 @@
-filterSelection("all"); // Execute the function and show all columns
-function filterSelection(c) {
-   var x, i;
-   x = document.getElementsByClassName("column");
-   if (c == "all") c = "";
-   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-   for (i = 0; i < x.length; i++) {
-      w3RemoveClass(x[i], "show");
-      if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-   }
-}
+let sortBtn = document.querySelector(".filter-menu").children;
+let sortItem = document.querySelector(".filter-item").children;
 
-// Show filtered elements
-function w3AddClass(element, name) {
-   var i, arr1, arr2;
-   arr1 = element.className.split(" ");
-   arr2 = name.split(" ");
-   for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {
-         element.className += " " + arr2[i];
+for (let i = 0; i < sortBtn.length; i++) {
+   sortBtn[i].addEventListener("click", function () {
+      for (let j = 0; j < sortBtn.length; j++) {
+         sortBtn[j].classList.remove("current");
       }
-   }
-}
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-   var i, arr1, arr2;
-   arr1 = element.className.split(" ");
-   arr2 = name.split(" ");
-   for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-         arr1.splice(arr1.indexOf(arr2[i]), 1);
+      this.classList.add("current");
+
+      let targetData = this.getAttribute("data-target");
+
+      for (let k = 0; k < sortItem.length; k++) {
+         sortItem[k].classList.remove("active");
+         sortItem[k].classList.add("delete");
+
+         if (sortItem[k].getAttribute("data-item") == targetData || targetData == "all") {
+            sortItem[k].classList.remove("delete");
+            sortItem[k].classList.add("active");
+         }
       }
-   }
-   element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn-filter");
-for (var i = 0; i < btns.length; i++) {
-   btns[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace("active", "");
-      this.className += " active";
    });
 }
